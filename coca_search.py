1be736ab0bc5b43593063c2257bf1e74742daabd
Helen <hyzhang@umass.edu>
2018-09-20 14:37:24 -0700

    Add files via upload
    updated to include all the adverb (r*) and adjective (j*) tags in the search

--- coca_search.py
+++ coca_search.py
@@ -22,61 +22,46 @@ df_2010 = pd.DataFrame()
 for filename in fnames:
     year = re.compile(r'\d+').findall(filename)
 
-    # for the old POS files
+    # for the OLD POS FILES
     # btw the year 2012 has 2 files (1 old, 1 new)
     if not filename[:4].isdigit():
         df_file = pd.read_csv(filename, encoding='latin1', skiprows=[0], sep='\t', error_bad_lines=False, quoting=3,
                               lineterminator='\r', header=None, index_col=None)
         df_file.columns = ['word', 'lemma', 'pos']
-        # add a column for the next word and POS
-        df_file["nextword"] = df_file["word"].shift(1)
-        df_file["nextpos"] = df_file['pos'].shift(1)
+        df_file = df_file[['word', 'pos']]
 
-        # search for POS adv (rr) ending in -ly followed by POS adj (jj)
-        # if the rr/jj is the first tag in an ambiguity tag pair
-        # filter for slashes or hyphens
-        df_file = df_file[(df_file['pos'].str[:2] == 'rr') & (df_file['word'].str[-2:] == 'ly') & (~df_file['word'].str.contains("-|/",  na=False))
-                      & (df_file['nextpos'].str[:2] == 'jj') & (~df_file['nextword'].str.contains("-|/",  na=False))]
-        df_file = df_file[['word', 'pos', 'nextword', 'nextpos']]
-
-        # trim a bit
+        # trim out the first character
         df_file['word'] = df_file['word'].str[1:]
-        df_file['nextword'] = df_file['nextword'].str[1:]
 
-        # lowercase
-        df_file['word'] = df_file['word'].str.lower()
-        df_file['nextword'] = df_file['nextword'].str.lower()
-
-        # add a column for the adv adj pair
-        df_file['pair'] = df_file['word'] + " " + df_file['nextword']
-        df_file = df_file.drop(columns=['word', 'pos', 'nextword', 'nextpos'], axis=1)
-
-
-    # for the newer POS files (after 2012)
+    # for the NEW POS FILES
     if filename[:4].isdigit():
-
         # newer POS files have different columns
         df_file = pd.read_csv(filename, encoding='latin1', skiprows=[0], sep='\t', error_bad_lines=False, quoting=3,
                               lineterminator='\r', header=None, index_col=None)
-        df_file.columns = ['index','textid', 'word', 'lemma', 'pos']
-        df_file["nextword"] = df_file["word"].shift(-1)
-        df_file["nextpos"] = df_file['pos'].shift(-1)
+        df_file.columns = ['index', 'textid', 'word', 'lemma', 'pos']
+        df_file = df_file[['word', 'pos']]
 
-        df_file = df_file[(df_file['pos'].str[:2] == 'rr') & (df_file['word'].str[-2:] == 'ly') & (~df_file['word'].str.contains("-|/", na=False))
-                          & (df_file['nextpos'].str[:2] == 'jj') & (~df_file['nextword'].str.contains("-|/", na=False))]
-        df_file = df_file[['word', 'pos', 'nextword', 'nextpos']]
+    # lowercase
+    df_file['word'] = df_file['word'].str.lower()
 
-        # eliminate first row
-        df_file = df_file.iloc[1:]
+    # add a column for the next word and POS
+    df_file["nextword"] = df_file["word"].shift(-1)
+    df_file["nextpos"] = df_file['pos'].shift(-1)
 
-        df_file['word'] = df_file['word'].str.lower()
-        df_file['nextword'] = df_file['nextword'].str.lower()
+    # filter for rows where the POS = adv (r*), word ends in -ly followed by NEXTPOS = adj (j*)
+    # NOTE: in ambiguity tag pairs the r*/j* will be the first tag
+    # filter for slashes or hyphens
+    df_file = df_file[(df_file['pos'].str[0] == 'r') & (df_file['word'].str[-2:] == 'ly') & (~df_file['word'].str.contains("-|/",  na=False))
+                      & (df_file['nextpos'].str[0] == 'j') & (~df_file['nextword'].str.contains("-|/",  na=False))]
 
-        df_file['pair'] = df_file['word'] + " " + df_file['nextword']
-        df_file = df_file.drop(columns=['word', 'pos', 'nextword', 'nextpos'], axis=1)
+    # add a column for the adv adj pair
+    df_file['pair'] = df_file['word'] + " " + df_file['nextword']
+    df_file = df_file.drop(columns=['word', 'pos', 'nextword', 'nextpos'], axis=1)
 
     # get the count for each pair
-    df_file['count'] = df_file.groupby('pair', as_index=False)['pair'].transform(lambda s: s.count())
+    #df_file['count'] = df_file.groupby('pair', as_index=False)['pair'].transform(lambda s: s.count())
+    #df_file['freq'] = df_file.groupby(["pair"]).count()
+    df_file['freq'] = df_file.groupby('pair')['pair'].transform('count')
 
     # get rid of duplicates
     df_file = df_file.drop_duplicates(subset=['pair'])
@@ -106,4 +91,13 @@ df_2005.to_csv(category + '_2005.csv', encoding='utf-8')
 df_2010.to_csv(category + '_2010.csv', encoding='utf-8')
 
 df_all.to_csv(category + '_all_years.csv', encoding='utf-8')
+
+
+# total counts for each period
+with open(category + "_totals" + ".txt", "w+") as f:
+    f.write("1990: " + str(df_1990['freq'].sum()) + "\n")
+    f.write("1995: " + str(df_1995['freq'].sum()) + "\n")
+    f.write("2000: " + str(df_2000['freq'].sum()) + "\n")
+    f.write("2005: " + str(df_2005['freq'].sum()) + "\n")
+    f.write("2010: " + str(df_2010['freq'].sum()) + "\n")
 
